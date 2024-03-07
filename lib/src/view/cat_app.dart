fe708f48fc31c199955760ce0cc3fab8ca7af7ec
Enes <enesakbal00@gmail.com>
2024-03-08 00:23:43 +0300

    ref: refactored CatApp to handle FetchCatsNoInternet state

--- lib/src/view/cat_app.dart
+++ lib/src/view/cat_app.dart
@@ -24,7 +24,6 @@ class _CatAppState extends State<CatApp> {
   @override
   void initState() {
     scrollController = ScrollController()..addListener(_scrollListener);
-
     context.read<FetchCatsCubit>().fetchCats();
 
     super.initState();
@@ -40,45 +39,54 @@ class _CatAppState extends State<CatApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Cat App')),
-      body: ListView(
-        shrinkWrap: true,
-        physics: const BouncingScrollPhysics(),
-        controller: scrollController,
-        children: [
-          GridView.builder(
-            shrinkWrap: true,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8,
-              mainAxisSpacing: 8,
-            ),
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: context.watch<FetchCatsCubit>().catList.length,
-            itemBuilder: (context, index) {
-              final item = context.watch<FetchCatsCubit>().catList[index];
+    return BlocListener<FetchCatsCubit, FetchCatsState>(
+      listener: (context, state) {
+        if (state is FetchCatsNoInternet) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('No internet connection')),
+          );
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Cat App')),
+        body: ListView(
+          shrinkWrap: true,
+          physics: const BouncingScrollPhysics(),
+          controller: scrollController,
+          children: [
+            GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+              ),
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: context.watch<FetchCatsCubit>().catList.length,
+              itemBuilder: (context, index) {
+                final item = context.watch<FetchCatsCubit>().catList[index];
 
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(color: Colors.grey),
-                    image: DecorationImage(
-                      image: NetworkImage(item.url ?? ''),
-                      fit: BoxFit.cover,
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(color: Colors.grey),
+                      image: DecorationImage(
+                        image: NetworkImage(item.url ?? ''),
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
-          ),
-          const SizedBox(
-            height: 100,
-            child: Center(child: CircularProgressIndicator.adaptive()),
-          ),
-        ],
+                );
+              },
+            ),
+            const SizedBox(
+              height: 100,
+              child: Center(child: CircularProgressIndicator.adaptive()),
+            ),
+          ],
+        ),
       ),
     );
   }
