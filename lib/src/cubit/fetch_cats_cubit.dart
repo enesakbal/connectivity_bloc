53cc2e1c2dfd84170512426e42102555aa29f650
Enes <enesakbal00@gmail.com>
2024-03-07 23:13:46 +0300

    feat: phase 1

--- lib/src/cubit/fetch_cats_cubit.dart
+++ lib/src/cubit/fetch_cats_cubit.dart
@@ -1,11 +1,12 @@
 import 'package:connectivity_bloc/core/models/cat_model.dart';
 import 'package:connectivity_bloc/core/service/cat_service.dart';
+import 'package:connectivity_bloc/src/connectivity/connectivity_mixin.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'fetch_cats_state.dart';
 
-class FetchCatsCubit extends Cubit<FetchCatsState> {
+class FetchCatsCubit extends Cubit<FetchCatsState> with ListenConnectivity<FetchCatsState, FetchCatsError> {
   FetchCatsCubit(this._catApiService) : super(const FetchCatsInitial());
 
   final List<CatModel> catList = [];
