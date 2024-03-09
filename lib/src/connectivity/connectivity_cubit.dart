4b1ffef6bade1b615c857c319840b94da4ae9d15
Enes <enesakbal00@gmail.com>
2024-03-09 20:46:57 +0300

    phase 2

--- lib/src/connectivity/connectivity_cubit.dart
+++ lib/src/connectivity/connectivity_cubit.dart
@@ -1,8 +1,7 @@
 import 'dart:async';
-import 'dart:developer';
 
+import 'package:connectivity_bloc/src/debouncer/debouncer.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 abstract class ConnectivityCubit<State> extends BlocBase<State> {
@@ -10,9 +9,11 @@ abstract class ConnectivityCubit<State> extends BlocBase<State> {
   late final StreamSubscription<ConnectivityResult> _connectivitySubscription;
 
   ConnectivityCubit(super.initialState) {
-    initConnectivity();
-
-    _connectivitySubscription = _connectivity.onConnectivityChanged.listen(onConnectivityChange);
+    _connectivity = Connectivity();
+    final debouncer = Debouncer(duration: const Duration(milliseconds: 1500));
+    _connectivitySubscription = _connectivity.onConnectivityChanged.listen((result) {
+      debouncer.run(() => onConnectivityChange.call(result));
+    });
   }
 
   void onConnectivityChange(ConnectivityResult result);
@@ -21,21 +22,5 @@ abstract class ConnectivityCubit<State> extends BlocBase<State> {
   Future<void> close() {
     _connectivitySubscription.cancel();
     return super.close();
-  }
-
-  Future<void> initConnectivity() async {
-    _connectivity = Connectivity();
-
-    ConnectivityResult result;
-    try {
-      result = await _connectivity.checkConnectivity();
-      // _isInitialised = true;
-    } on PlatformException catch (e) {
-      log('Couldn\'t check connectivity status', error: e);
-      return;
-    }
-
-    onConnectivityChange(result);
-    return;
   }
 }
